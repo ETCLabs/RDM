57f1dac7dc69ac4ec33becd0561b5b27294d7ef7
Travis CI <travis@travis-ci.org>
2019-04-05 23:50:53 +0000

    Deploy code docs to GitHub Pages Travis build: 10
    Commit: ca219e92c3215d24d93310e136c070af3e47ee28

--- docs/head/group__controller.js
+++ docs/head/group__controller.js
@@ -1,5 +1,6 @@
 var group__controller =
 [
     [ "rdmctl_create_command", "group__controller.html#ga8dd7e45b8f81aee38b235c2fb2152f0a", null ],
+    [ "rdmctl_is_non_disc_response", "group__controller.html#ga62a94b697a099eea6bad03f10a592277", null ],
     [ "rdmctl_unpack_response", "group__controller.html#gaa38b3690b6efebbc86cd2a6d51bbe2ad", null ]
 ];
--- docs/head/responder_8h.js
+++ docs/head/responder_8h.js
@@ -1,5 +1,6 @@
 var responder_8h =
 [
     [ "rdmresp_create_response", "group__responder.html#gac086fc597854cdb099737a66dc457344", null ],
+    [ "rdmresp_is_non_disc_command", "group__responder.html#ga4f3587c5330568a286873f8d392d8233", null ],
     [ "rdmresp_unpack_command", "group__responder.html#ga712f0a7856b5916073ac5efda65f0683", null ]
 ];
