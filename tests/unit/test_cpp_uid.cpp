a61fb64128408ee5c9101fb5ff671e21cb86be8a
Sam <samuelmkearney@gmail.com>
2020-06-05 09:05:25 -0500

    Update license header

--- tests/unit/test_cpp_uid.cpp
+++ tests/unit/test_cpp_uid.cpp
@@ -1,5 +1,5 @@
 /******************************************************************************
- * Copyright 2019 ETC Inc.
+ * Copyright 2020 ETC Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,10 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- *******************************************************************************
+ ******************************************************************************
  * This file is a part of RDM. For more information, go to:
  * https://github.com/ETCLabs/RDM
- ******************************************************************************/
+ *****************************************************************************/
 
 #include "rdm/cpp/uid.h"
 #include "gtest/gtest.h"
