a75c3598873f9d7faf5572d1fcdeb24c67722817
Sam <samuelmkearney@gmail.com>
2020-07-02 20:32:59 -0500

    Update version files for RDM build 0.2.0.9

--- include/rdm/version.h
+++ include/rdm/version.h
@@ -46,7 +46,7 @@
 #define RDM_VERSION_MAJOR 0 /**< The major version. */
 #define RDM_VERSION_MINOR 2 /**< The minor version. */
 #define RDM_VERSION_PATCH 0 /**< The patch version. */
-#define RDM_VERSION_BUILD 8 /**< The build number. */
+#define RDM_VERSION_BUILD 9 /**< The build number. */
 /**
  * @}
  */
@@ -55,8 +55,8 @@
  * @name RDM version strings
  * @{
  */
-#define RDM_VERSION_STRING "0.2.0.8"
-#define RDM_VERSION_DATESTR "29.Jun.2020"
+#define RDM_VERSION_STRING "0.2.0.9"
+#define RDM_VERSION_DATESTR "02.Jul.2020"
 #define RDM_VERSION_COPYRIGHT "Copyright 2020 ETC Inc."
 #define RDM_VERSION_PRODUCTNAME "RDM"
 /**
